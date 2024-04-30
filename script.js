43ae43f76000cc6f446809e8b67c27d2e99335f6
Atilla Çelik <celikatilla89@gmail.com>
2024-04-30 16:25:44 +0200

    Added the show library feature. Code is really messy and needs refactoring.

--- script.js
+++ script.js
@@ -23,40 +23,121 @@ const Book = function (title, author, pages, read) {
     this.read = read;
 };
 
-const showLibrary = function () {
-    let bookShowCase = document.createElement("div");
-    bookShowCase.classList.add("book");
-    bookShowCase.innerText = `Title: ${bookTitle.value}\nAuthor: ${author.value}\nPages: ${pages.value}`;
-    books.appendChild(bookShowCase);
-};
-
-// const addToLib = function (book) {
-//     book.
-// }
-
-const meridian = new Book("Blood Meridian", "Cormac Mc Carthy", "300", "no");
-
+//Opens the window for adding a new book
+//************************************* */
 newBook.addEventListener("click", () => {
     formContainer.classList.remove("hidden");
 });
 
+//Cancels and closes the new book window
+//************************************* */
+cancel.addEventListener("click", () => {
+    formContainer.classList.add("hidden");
+});
+
+//Adds the book to the library and displays it
+//************************************* */
 addBook.addEventListener("click", () => {
     let info = [bookTitle.value, author.value, pages.value, read.value];
     let book = new Book(...info);
     library.push(book);
-    // let bookShowCase = document.createElement("div");
-    // bookShowCase.innerHTML = `${book}`;
-    showLibrary();
-    bookTitle.value = "";
-    author.value = "";
-    pages.value = "";
-    read.value = "";
+
+    // bookCont = document.createElement("div");
+    // bookCont.classList.add("book-cont");
+
+    // //Creates a div for the button to be put into
+    // let deleteBtnCont = document.createElement("div");
+    // deleteBtnCont.classList.add("delete-btn-cont");
+
+    // //Creates the button to delete the "book"
+    // deleteBtn = document.createElement("button");
+    // deleteBtn.innerText = "X";
+    // deleteBtn.classList.add("delete-btn");
+    // // deleteBtn.addEventListener("click", () => {
+    // //     bookCont.remove();
+    // // });
+
+    //Appends the button to the div
+    // deleteBtnCont.appendChild(deleteBtn);
+
+    // //Creates the div for each "book" that is added
+    // let bookInfo = document.createElement("div");
+    // bookInfo.classList.add("book");
+
+    // //Tries to add the div with the delete button to the "book"
+    // bookCont.appendChild(deleteBtnCont);
+
+    // //"books" is the basically the area where the "book" items are being shown. This part works fine
+    // bookInfo.innerText = `Title: ${bookTitle.value}\n\nAuthor: ${author.value}\n\nPages: ${pages.value}`;
+    // bookCont.appendChild(bookInfo);
+    // books.appendChild(bookCont);
+    // bookCont.dataset.number = library.length;
+    // libraryBtns();
+    // bookTitle.value = "";
+    // author.value = "";
+    // pages.value = "";
+    // read.value = "";
+    displayLibrary();
 
     formContainer.classList.add("hidden");
 
     console.log(library);
 });
 
-cancel.addEventListener("click", () => {
-    formContainer.classList.add("hidden");
-});
+const displayLibrary = function () {
+    while (books.firstChild) {
+        books.firstChild.remove();
+    }
+    library.forEach((book) => {
+        // book.remove();
+        //!!!!!!!!!
+
+        let bookCont = document.createElement("div");
+        bookCont.classList.add("book-cont");
+        bookCont.dataset.bookNumber = library.length;
+
+        //Creates a div for the delete button to be put into
+        let deleteBtnCont = document.createElement("div");
+        deleteBtnCont.classList.add("delete-btn-cont");
+
+        //Creates the button to delete the "book"
+        let deleteBtn = document.createElement("button");
+        deleteBtn.innerText = "X";
+        deleteBtn.classList.add("delete-btn");
+        deleteBtn.addEventListener("click", () => {
+            bookCont.remove();
+        });
+
+        // deleteBtn.addEventListener("click", () => {
+        //     bookCont.remove();
+        // });
+        deleteBtnCont.appendChild(deleteBtn);
+        bookCont.appendChild(deleteBtnCont);
+
+        //Creates the div for each "book" that is added
+        let bookInfo = document.createElement("div");
+        bookInfo.classList.add("book");
+
+        //Tries to add the div with the delete button to the "book"
+
+        //"books" is the basically the area where the "book" items are being shown. This part works fine
+        bookInfo.innerText = `Title: ${book.title}\n\nAuthor: ${book.author}\n\nPages: ${book.pages}`;
+        bookCont.appendChild(bookInfo);
+        books.appendChild(bookCont);
+        bookCont.dataset.number = library.length;
+
+        bookTitle.value = "";
+        author.value = "";
+        pages.value = "";
+        read.value = "";
+        console.log(bookCont.dataset.bookNumber);
+    });
+};
+
+// const libraryBtns = function () {
+//     library.forEach((book) => {
+//         deleteBtn.addEventListener("click", () => {
+//             bookCont.dataset.bookNumber.remove();
+//         });
+//     });
+// };
